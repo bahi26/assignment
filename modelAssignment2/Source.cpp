de6c3d02402b017fa28cf11b595a75b02cbc4f1e
bahi26 <bahi.ali26@yahoo.com>
2018-11-27 09:28:57 +0200

    last

--- modelAssignment2/Source.cpp
+++ modelAssignment2/Source.cpp
@@ -170,111 +170,124 @@ int main()
 	c=constructMatrix(m, n);
 	d=constructMatrix(m, m);
 	int j = 0;
-	int k = 20;
+	int k = 19;
 	double vd = 0,id=0;
+
+	Matrix::initilzeMatrix(z, n + m, 1);
+	Matrix::initilzeMatrix(g, n , n);
+	Matrix::initilzeMatrix(b, n , m);
+	Matrix::initilzeMatrix(c, m, n);
+	Matrix::initilzeMatrix(d,  m, m);
+	j = 0;
+	for (int i = 0; i < elements.size(); ++i)
+	{
+		if (elements[i].type == 3)//resistance fill g
+		{
+			if (elements[i].src>0)
+				g[elements[i].src - 1][elements[i].src - 1] += 1 / elements[i].value;
+			if (elements[i].dst>0)
+				g[elements[i].dst - 1][elements[i].dst - 1] += 1 / elements[i].value;
+
+			if (elements[i].src>0 && elements[i].dst>0)
+			{
+				g[elements[i].src - 1][elements[i].dst - 1] -= 1 / elements[i].value;
+				g[elements[i].dst - 1][elements[i].src - 1] -= 1 / elements[i].value;
+			}
+		}
+		if (elements[i].type == 1 || elements[i].type == 4)//voltage src or inductor
+		{
+			if (elements[i].src>0)
+				b[elements[i].src - 1][j] = c[j][elements[i].src - 1] = 1;
+			if (elements[i].dst>0)
+				b[elements[i].dst - 1][j] = c[j][elements[i].dst - 1] = -1;
+
+			if (elements[i].type == 4)//inductor
+			{
+				d[j][j] -= elements[i].value / h;
+
+				id = elements[i].intial_value;	
+
+				z[n + j][0] = id* elements[i].value / h;//
+			}
+			else
+				z[n + j][0] = elements[i].value;
+			++j;
+
+		}
+		if (elements[i].type == 5)//caps
+		{
+			if (elements[i].src>0)
+				g[elements[i].src - 1][elements[i].src - 1] += elements[i].value / h;
+			if (elements[i].dst>0)
+				g[elements[i].dst - 1][elements[i].dst - 1] += elements[i].value / h;
+
+			if (elements[i].src>0 && elements[i].dst>0)
+			{
+				g[elements[i].src - 1][elements[i].dst - 1] -= elements[i].value / h;
+				g[elements[i].dst - 1][elements[i].src - 1] -= elements[i].value / h;
+			}
+			vd = elements[i].intial_value;
+
+			if (elements[i].src>0)
+				z[elements[i].src - 1][0] += vd* elements[i].value / h;
+			if (elements[i].dst>0)
+				z[elements[i].dst - 1][0] -= vd* elements[i].value / h;
+		}
+		if (elements[i].type == 2)
+		{
+			if (elements[i].src>0)
+				z[elements[i].src - 1][0] += elements[i].value;
+			if (elements[i].dst>0)
+				z[elements[i].dst - 1][0] -= elements[i].value;
+		}
+
+	}
+
+	concatenatetMatrix(g, b, c, d, n, m, A);
+	Matrix::MatrixInversion(A,n+m ,invA);
+	Matrix::MatrixMultiplication(invA, n + m, z, x);
+	printMat(x, n + m, 1);
+
+	//the remining iterations
 	while (--k)
 	{
-		Matrix::initilzeMatrix(z, n + m, 1);
-		Matrix::initilzeMatrix(g, n , n);
-		Matrix::initilzeMatrix(b, n , m);
-		Matrix::initilzeMatrix(c, m, n);
-		Matrix::initilzeMatrix(d,  m, m);
 		j = 0;
 		for (int i = 0; i < elements.size(); ++i)
 		{
-			if (elements[i].type == 3)
+			if (elements[i].type == 5)//if type is cap
 			{
-				if (elements[i].src>0)
-					g[elements[i].src - 1][elements[i].src - 1] += 1 / elements[i].value;
-				if (elements[i].dst>0)
-					g[elements[i].dst - 1][elements[i].dst - 1] += 1 / elements[i].value;
-
-				if (elements[i].src>0 && elements[i].dst>0)
-				{
-					g[elements[i].src - 1][elements[i].dst - 1] -= 1 / elements[i].value;
-					g[elements[i].dst - 1][elements[i].src - 1] -= 1 / elements[i].value;
-				}
-			}
-			if (elements[i].type == 1 || elements[i].type == 4)
-			{
-				if (elements[i].src>0)
-					b[elements[i].src - 1][j] = c[j][elements[i].src - 1] = 1;
-				if (elements[i].dst>0)
-					b[elements[i].dst - 1][j] = c[j][elements[i].dst - 1] = -1;
-
-				if (elements[i].type == 4)
-				{
-					d[j][j] -= elements[i].value / h;
-
-					if (k == 19)
-						vd = 0;
-					
-					else
-					{
-						double vds, vdd = 0;
-						if (elements[i].dst == 0)
-							vdd = 0;
-						else
-							vdd = x[elements[i].dst - 1][0];
-						if (elements[i].src == 0)
-							vds = 0;
-						else
-							vds = x[elements[i].src - 1][0];
-						vd = vds - vdd;
-					}
-						
-
-					z[n + j][0] = vd* elements[i].value / h;//
-				}
+			
+				double vds, vdd = 0;
+				if (elements[i].dst == 0)
+					vdd = 0;
 				else
-					z[n + j][0] = elements[i].value;
+					vdd = x[elements[i].dst - 1][0];
+				if (elements[i].src == 0)
+					vds = 0;
+				else
+					vds = x[elements[i].src - 1][0];
+				vd = vds - vdd;
+				if (elements[i].src>0)
+					z[elements[i].src - 1][0] += vd* elements[i].value / h;
+				if (elements[i].dst>0)
+					z[elements[i].dst - 1][0] -= vd* elements[i].value / h;
+				
+			}
+			if (elements[i].type == 4)//ind
+			{
+				id = x[i][0];
+				z[n + j][0] = id* elements[i].value / h;//
+			}
+			if (elements[i].type == 4 || elements[i].type == 1)
 				++j;
-
-			}
-			if (elements[i].type == 4 || elements[i].type == 5)
-			{
-				if (elements[i].src>0)
-					g[elements[i].src - 1][elements[i].src - 1] += elements[i].value / h;
-				if (elements[i].dst>0)
-					g[elements[i].dst - 1][elements[i].dst - 1] += elements[i].value / h;
-
-				if (elements[i].src>0 && elements[i].dst>0)
-				{
-					g[elements[i].src - 1][elements[i].dst - 1] -= elements[i].value / h;
-					g[elements[i].dst - 1][elements[i].src - 1] -= elements[i].value / h;
-				}
-			}
-			if (elements[i].type == 2)
-			{
-				if (elements[i].src>0)
-					z[elements[i].src - 1][0] += elements[i].value;
-				if (elements[i].dst>0)
-					z[elements[i].dst - 1][0] -= elements[i].value;
-			}
-
-			if (elements[i].type == 5)
-			{
-				if (k == 19)
-					id = 0;
-				else
-					id = x[i][0];
-				if (elements[i].src>0)
-					z[elements[i].src - 1][0] += id* elements[i].value / h;
-				if (elements[i].dst>0)
-					z[elements[i].dst - 1][0] -= id* elements[i].value / h;
-			}
-
 		}
-
 		concatenatetMatrix(g, b, c, d, n, m, A);
-		Matrix::MatrixInversion(A,n+m ,invA);
+		Matrix::MatrixInversion(A, n + m, invA);
 		Matrix::MatrixMultiplication(invA, n + m, z, x);
-		printMat(x, n + m, 1);
-		break;
 	}
-		/*printMat(g, n, n);
-		printMat(b, n, m );
-		printMat(c, m, n );
-		printMat(d, m, m);*/
-	printMat(x, n+m,1);
+	/*printMat(g, n, n);
+	printMat(b, n, m );
+	printMat(c, m, n );
+	printMat(d, m, m);*/
+	//printMat(x, n+m,1);
 }
